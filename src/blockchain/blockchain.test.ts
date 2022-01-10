bfb19741aa8549c62d3eacf02ca2457acd1ab546
j-eugenio <eugeniopb4@gmail.com>
2022-01-10 10:09:15 -0300

    Testes de validacao dos blocks e das chains

--- src/blockchain/blockchain.test.ts
+++ src/blockchain/blockchain.test.ts
@@ -3,9 +3,11 @@ import { Blockchain } from './';
 
 describe('Blockchain', () => {
   let blockchain: Blockchain;
+  let blockchain2: Blockchain;
 
   beforeEach(() => {
     blockchain = new Blockchain;
+    blockchain2 = new Blockchain;
   });
 
   it('starts witch genesis block', () => {
@@ -17,5 +19,23 @@ describe('Blockchain', () => {
     blockchain.addBlock(data);
 
     expect(blockchain.chain.slice(-1)[0].data).toEqual(data)
+  });
+
+  it('validates a valid chain', () => {
+    blockchain2.addBlock('500U$');
+
+    expect(blockchain.isValidChain(blockchain2.chain)).toBe(true)
+  });
+
+  it('invalidates a chain with a corrupt genesis block', () => {
+    blockchain2.chain[0].data = '0U$';
+    expect(blockchain.isValidChain(blockchain2.chain)).toBe(false)
+  });
+
+  it('invalidates a corrupt chain', () => {
+    blockchain2.addBlock('200U$');
+    blockchain2.chain[1].data = '0U$';
+
+    expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
   });
 })
