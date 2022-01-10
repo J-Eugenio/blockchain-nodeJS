0e8969d9bd5e0891e4b6e497224aafba0bb610ae
j-eugenio <eugeniopb4@gmail.com>
2022-01-10 11:17:42 -0300

    POST: /mine: Mine blocks

--- src/app/index.ts
+++ src/app/index.ts
@@ -6,9 +6,24 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+app.use(express.json());
+
 app.get('/blocks', (request:Request, response: Response) => {
-  return response.json(bc.chain);
-})
+  response.json(bc.chain);
+});
+
+app.post('/mine', (request:Request, response: Response) => {
+  const { data } = request.body;
+
+  if(!data){
+    return response.status(400).json('Data not found')
+  }
+  
+  const block = bc.addBlock(data);
+  console.log(`New Block added: ${block.toString()}`);
+  
+  response.redirect('/blocks');
+});
 
 app.listen(HTTP_PORT, () => {
   console.log(`Server Running on port ${HTTP_PORT}!!!`)
